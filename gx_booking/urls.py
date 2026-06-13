9a0a6de7d1f28d4e9fc436ce8a9f00deb772e9d8
parkran-gx <parkran.gx@gmail.com>
2026-06-13 14:16:40 +0000

    fix: urls.py 중복 namespace 제거

--- gx_booking/urls.py
+++ gx_booking/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('notices/', include('apps.notices.urls')),
     path('messages/', include('apps.messages_app.urls')),
     path('enrollments/', include('apps.enrollments.urls')),
-    path('manage/', include('apps.classes.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
