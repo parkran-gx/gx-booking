60bfd27070ff1e04dcd41934f17e969f9a37adee
parkran-gx <parkran.gx@gmail.com>
2026-06-14 23:16:54 +0000

    fix: settings_prod CSRF 설정 수정

--- gx_booking/settings_prod.py
+++ gx_booking/settings_prod.py
@@ -2,13 +2,13 @@ from .settings import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['parkrangx.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ['https://parkrangx.pythonanywhere.com']
 
-# 보안 설정
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_HTTPONLY = False
 
-# 정적 파일
 STATIC_ROOT = BASE_DIR / 'staticfiles'
