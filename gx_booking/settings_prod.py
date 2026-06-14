514fae8cb563bf365f57b42e7e78c201596d3949
parkran-gx <parkran.gx@gmail.com>
2026-06-14 23:26:53 +0000

    feat: C팔레트 크림·모브로즈 색감 적용

--- gx_booking/settings_prod.py
+++ gx_booking/settings_prod.py
@@ -3,12 +3,10 @@ from .settings import *
 DEBUG = False
 ALLOWED_HOSTS = ['parkrangx.pythonanywhere.com']
 CSRF_TRUSTED_ORIGINS = ['https://parkrangx.pythonanywhere.com']
-
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-X_FRAME_OPTIONS = 'DENY'
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_HTTPONLY = False
-
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
